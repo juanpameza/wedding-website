cd535c7b9a23380bb5b1f64ed85a770bd1b14409
juanpameza <juan@atma-energy.com>
2026-05-09 14:32:38 -0500

    fix: use @/ alias for keystatic config imports, set localBaseDirectory

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,4 +1,7 @@
 import { makeRouteHandler } from "@keystatic/next/route-handler";
-import config from "../../../../keystatic.config";
+import config from "@/keystatic.config";
 
-export const { GET, POST } = makeRouteHandler({ config });
+export const { GET, POST } = makeRouteHandler({
+  config,
+  localBaseDirectory: process.cwd(),
+});
